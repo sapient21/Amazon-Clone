454530ad25586739ef0ca3ec7139a9ff0908c54e
Sapient <sapientrules@gmail.com>
2025-11-02 20:57:36 +0300

    Use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,4 @@
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 3.5,
-      count: 87,
-    },
-    priceCents: 1090,
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 3.5,
-      count: 872,
-    },
-    priceCents: 2068,
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4,
-      count: 23,
-    },
-    priceCents: 799,
-  },
-];
+
 
 let productsHTML = '';
 
